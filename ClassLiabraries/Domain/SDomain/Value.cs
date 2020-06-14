6cdc48942e5ef896b9092668e8fb05d0cee27454
mahesh <mahesh.net2694@gmail.com>
2020-06-13 23:48:10 -0400

    Revert "Revert "Revert "Revert "test commit""""
    This reverts commit 1c71c7c1bb25944d8a78da80897bdc8e56b53bd5.

--- ClassLiabraries/Domain/SDomain/Value.cs
+++ ClassLiabraries/Domain/SDomain/Value.cs
@@ -6,7 +6,7 @@ namespace SDomain
     {
         public int ID { get; set; }
         public String Name { get; set; }
-        public String Email { get; set; }
+        public string email { get; set; }
 
     }
 }
